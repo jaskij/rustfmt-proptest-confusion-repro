5a7b5aa944f4229f99e48ba76f044aaa83987b33
Jan Dorniak <jdorniak@ultima-automatyka.pl>
2024-12-17 16:55:55 +0100

    more examples

---
+++ src/old-formats.rs
@@ -0,0 +1,25 @@
+mod doesnt_format;
+mod formats;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct Item {
+    pub first_long_name_a: u16,
+    pub item_count: u16,
+    pub area: u16,
+    pub address: u16,
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+
+    use super::Item;
+
+    proptest! {
+               #[test]
+               fn read_item_chars(s in "\\PC*") {
+                   let pi = Item{
+        first_long_name_a: 0,item_count: 0,area: 0,address: 0,};
+               }
+           }
+}
